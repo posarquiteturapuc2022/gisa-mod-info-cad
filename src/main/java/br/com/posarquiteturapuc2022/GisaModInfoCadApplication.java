b8ac72783bab21bfab89bb7540d6dfd97154e0d0
emaintenance <88213497+emaintenanceapp@users.noreply.github.com>
2022-04-20 15:14:39 -0300

    first commit

---
+++ src/main/java/br/com/posarquiteturapuc2022/GisaModInfoCadApplication.java
@@ -0,0 +1,13 @@
+package br.com.posarquiteturapuc2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GisaModInfoCadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GisaModInfoCadApplication.class, args);
+	}
+
+}
